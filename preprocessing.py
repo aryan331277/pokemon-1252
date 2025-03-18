66105142743702031ea650298bd32a2f33b0f839
Aryan Karmore <bt24csd009@iiitn.ac.in>
2025-03-18 21:47:10 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,5 +7,6 @@ for path in tqdm(image_paths):
     img = load_img(path, target_size=IMAGE_SIZE)  # Resize images
     img = img_to_array(img)  # Convert to NumPy array
     train_images.append(img)
+    
 train_images = np.array(train_images)
-train_images=(train_images-511.5)/511.5
+train_images=(train_image/255
