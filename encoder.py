6d8c6523d7d5124852b2615780d5d89cffdd656a
Aryan Karmore <bt24csd009@iiitn.ac.in>
2025-03-18 21:51:31 +0530

    Create encoder.py

---
+++ encoder.py
@@ -0,0 +1,11 @@
+from tensorflow.keras import layers, Model, Sequential
+
+LATENT_DIM = 100  
+
+encoder = Sequential(name="encoder")
+encoder.add(layers.Conv2D(64, (4, 4), strides=2, padding="same", activation="relu", input_shape=(256, 256, 3)))  # 128x128
+encoder.add(layers.Conv2D(128, (4, 4), strides=2, padding="same", activation="relu"))  # 64x64
+encoder.add(layers.Conv2D(256, (4, 4), strides=2, padding="same", activation="relu"))  # 32x32
+encoder.add(layers.Conv2D(512, (4, 4), strides=2, padding="same", activation="relu"))  # 16x16
+encoder.add(layers.Flatten())
+
